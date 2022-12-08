79921678f0b2fb43de7d2a1884c917d8a770c8ed
matheus-fongaro <matheus.fongaro@gmail.com>
2022-12-08 17:34:46 -0400

    refactor: aula 03-02

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import './styles/global.scss'
 import { RepositoryList } from './components/RepositoryList'
+
+import './styles/global.scss'
 
 export function App () {
     return (
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,7 @@
-import '../styles/repositories.scss'
+import { useEffect, useState } from 'react'
 import { RepositoryItem } from "./RepositoryItem"
+
+import '../styles/repositories.scss'
 
 const repository = {
     name: 'Unform',
@@ -7,10 +9,20 @@ const repository = {
     link: 'https://github.com/unform/unform'
 }
 
+// url da rocketseat https://api.github.com/orgs/rocketseat/repos
+
 export function RepositoryList () {
+    const [repositories, setRespositories] = useState([])
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRespositories(data))
+    }, [])
+
     return (
         <section className="repository-list">
-            <h1>Lista de repositórios</h1>
+            <h1>Lista de repositórios da Rocketseat</h1>
 
             <ul>
                 <RepositoryItem repository={repository} />
